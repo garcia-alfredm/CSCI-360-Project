2f7988402eb2c26c586aec30fcacf6924c0a1a18
Adam Parys <50283819+aparys@users.noreply.github.com>
2020-03-12 23:32:23 -0400

    Create CPPReader.js
    Added CPPReader.js
    Contains regular expressions to parse through the c++ code and a funtion to get the first line of c++ code and a function to remove the first line. Not yet tested.

---
+++ CPPReader.js
@@ -0,0 +1,32 @@
+var ifRegEx = /if\(.*?\)\s*\{?/; //matches if(anything) and any white space and optional "{"
+var elseRegEx = /else\s*\{?/;    //matches else and optional "{"
+var functionHeaderRegEx = /(\w+\s+)?\w+\s+\w+\s*\(\s*\w+\s+\w+\s*(,\s*\w+\s+\w+\s*)*\{/; //matches optional access modifier with 
+    //at least one white space and return type with at least one white space and function name with any number of white space 
+    //and "(" with any number of white space and any number of parameters (any word with at least one white space and anyword 
+    //with any number of white space) separated by ","and ")" and any number of white space and "{"
+var forLoopRegEx = /for\s*(.*)\s*\{?/; //matches for(anyting) and optional "{"
+var instructionRegEx = /.*;/; //matches anything ending with ";"
+                            //only useful when all other regular expressions failed
+var closeBracketRegEx =/\}/;  
+
+var getFirstLine = function(cppCode){
+    var a = regex1.exec(str1)[0];
+    cppCode = cppCode.replace(/^\s+/g, ""); //removes leading whitspace
+    if(cppCode.search(ifRegEx) == 0){
+        return ifRegEx.exec(cppCode)[0];
+    }else if(cppCode.search(elseRegEx)){
+        return elseRegEx.exec(cppCode)[0];
+    }else if(cppCode.search(functionHeaderRegEx)){
+        return functionHeaderRegEx.exec(cppCode)[0];
+    }else if(cppCode.search(forLoopRegEx)){
+        return forLoopRegEx.exec(cppCode)[0];
+    }else if(cppCode.search(closeBracketRegEx)){
+        return closeBracketRegEx.exec(cppCode)[0];
+    }else if(cppCode.search(instructionRegEx)){
+        return instructionRegEx.exec(cppCode)[0];
+    }else return "";
+}
+
+var removeFirstLine = function(cppCode){
+    cppCode = cppCode.replace(getFirstLine(cppCode),"");
+}
